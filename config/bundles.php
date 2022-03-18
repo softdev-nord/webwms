94945a497776b351b81d76cb53c8adb3cb8348e6
info@softdev-nord.de <info@softdev-nord.de>
2022-03-18 10:54:14 +0100

    WEBWMS-15 Symfony Update 5.4.*, SonataAdminBundle, Ordner für weitere WebWMS Bundles und StockInventoryBundle hinzugefügt

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,12 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    WebWMS\Bundles\StockInventoryBundle\StockInventoryBundle::class => ['all' => true],
+    Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
+    Sonata\Doctrine\Bridge\Symfony\SonataDoctrineBundle::class => ['all' => true],
+    Sonata\Form\Bridge\Symfony\SonataFormBundle::class => ['all' => true],
+    Sonata\Twig\Bridge\Symfony\SonataTwigBundle::class => ['all' => true],
+    Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
 ];
---
+++ custom/plugins/StockInventoryBundle/StockInventoryBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebWMS\Bundles\StockInventoryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StockInventoryBundle extends Bundle
+{
+
+}
