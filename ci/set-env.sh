cb498daf5ca137e6a4b73d8ed5d140fae7ef4d24
Rene Irrgang <info@softdev-nord.de>
2024-02-07 17:39:51 +0100

    WEBWMS-15 Anpassungen Pipeline

--- ci/set-env.sh
+++ ci/set-env.sh
@@ -4,9 +4,9 @@ set -Eeuo pipefail
 shopt -s inherit_errexit
 
 # Define variables
-export APP_ENV=prod
+export APP_ENV=${APP_ENV}
 export APP_SECRET=af2b45ff237087d068938bb0858bddff
-export APP_DEBUG=true
+export APP_DEBUG=${APP_DEBUG}
 
 # Generate environment variables file
 envsubst < .env.template > .env
