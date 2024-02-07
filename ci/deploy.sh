abf0ff487b856dfc94749ab566c3c1c1b806274b
Rene Irrgang <info@softdev-nord.de>
2024-02-07 14:00:50 +0100

    WEBWMS-15 Anpassungen Pipeline

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,7 +9,7 @@ rm -rf ${ENVIRONMENT_DIR}/
 mkdir ${ENVIRONMENT_DIR}
 
 cd ${ENVIRONMENT_DIR}
-git clone -b ${BITBUCKET_BRANCH} https://${BITBUCKET_USER}:${BITBUCKET_PASSWORD}@bitbucket.org/softdev-nord/webwms.git
+git clone -b ${BITBUCKET_BRANCH} https://${BITBUCKET_USER}:${BITBUCKET_PASSWORD}@bitbucket.org/softdev-nord/webwms.git .
 
 /opt/plesk/php/8.1/bin/php /usr/lib/plesk-9.0/composer.phar install --ignore-platform-reqs --no-scripts && \
 /opt/plesk/php/8.1/bin/php bin/console cache:clear
