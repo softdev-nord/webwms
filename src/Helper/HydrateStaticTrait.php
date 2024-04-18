927715ec8d4f68c59fe630edc8e4d77ff3f592d4
Rene Irrgang <info@softdev-nord.de>
2024-04-18 14:51:59 +0200

    WEBWMS-32 Helper hinzugefügt

---
+++ src/Helper/HydrateStaticTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebWMS\Helper;
+
+/**
+ * @package:    WebWMS\Helper
+ * @author:     SoftDev Nord, Rene Irrgang
+ * @copyright:  Copyright © 2019-2023, SoftDev Nord
+ */
+trait HydrateStaticTrait
+{
+    public static function hydrate(array $values): self
+    {
+        $dto = new self();
+
+        foreach ($values as $key => $value) {
+            if (property_exists($dto, $key)) {
+                $dto->$key = $value;
+            }
+        }
+
+        return $dto;
+    }
+}
