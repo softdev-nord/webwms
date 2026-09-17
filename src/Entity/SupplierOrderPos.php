bd79aafa6e33052507a97678232663db811ac4aa
Rene <69515257+rirrgang1281@users.noreply.github.com>
2026-09-17 11:29:02 +0200

    WEBWMS-3.0.0 migrate serializer group attributes

--- src/Entity/SupplierOrderPos.php
+++ src/Entity/SupplierOrderPos.php
@@ -14,7 +14,7 @@ use ApiPlatform\Metadata\Put;
 use DateTimeInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Attribute\Groups;
 use WebWMS\Helper\Attribute\ClassInformation;
 use WebWMS\Repository\SupplierOrderPosRepository;
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,7 +18,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Stringable;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
-use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 use WebWMS\Helper\Attribute\ClassInformation;
 use WebWMS\Repository\UserRepository;
